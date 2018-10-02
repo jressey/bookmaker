5f098802aff05c0df9300c653d937175d7201cc3
Joe Essey <joe.essey@salesloft.com>
2018-10-01 20:57:48 -0400

    update team model

--- lib/bookmaker/team.ex
+++ lib/bookmaker/team.ex
@@ -4,9 +4,21 @@ defmodule Bookmaker.Team do
 
 
   schema "teams" do
-    field :losses, :integer
+    field :api_id, :string
     field :name, :string
+    field :market, :string
+    field :alias, :string
+    #sports_radar_id
+    field :sr_id, :string
     field :wins, :integer
+    field :losses, :integer
+    field :ties, :integer
+    field :win_pct, :float
+    field :points_for, :integer
+    field :points_against, :integer
+    field :points_rank, :integer
+    field :touchdown_diff, :integer
+    field :conf_h2h, :integer
 
     timestamps()
   end
@@ -14,7 +26,19 @@ defmodule Bookmaker.Team do
   @doc false
   def changeset(team, attrs) do
     team
-    |> cast(attrs, [:name, :wins, :losses])
-    |> validate_required([:name, :wins, :losses])
+    |> cast(attrs,
+      [
+        :api_id, :name, :market, :alias,
+        :sr_id, :wins, :losses, :ties, :win_pct,
+        :points_for, :points_against, :points_rank,
+        :touchdown_diff, :conf_h2h
+      ])
+    |> validate_required(
+      [
+        :api_id, :name, :market, :alias,
+        :sr_id, :wins, :losses, :ties, :win_pct,
+        :points_for, :points_against, :points_rank,
+        :touchdown_diff, :conf_h2h
+      ])
   end
 end
--- priv/repo/migrations/20181001234219_create_teams.exs
+++ priv/repo/migrations/20181001234219_create_teams.exs
@@ -3,9 +3,21 @@ defmodule Bookmaker.Repo.Migrations.CreateTeams do
 
   def change do
     create table(:teams) do
+      add :api_id, :string
       add :name, :string
+      add :market, :string
+      add :alias, :string
+      #sports_radar_id
+      add :sr_id, :string
       add :wins, :integer
       add :losses, :integer
+      add :ties, :integer
+      add :win_pct, :float
+      add :points_for, :integer
+      add :points_against, :integer
+      add :points_rank, :integer
+      add :touchdown_diff, :integer
+      add :conf_h2h, :integer
 
       timestamps()
     end
