76a1928b5c52b0e8e3aef48edde1643bf2834ff2
Joe Essey <joe.essey@salesloft.com>
2018-10-07 12:59:48 -0400

    update pool size to be variable

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -19,5 +19,5 @@ config :bookmaker, BookmakerWeb.Endpoint,
 config :bookmaker, Bookmaker.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
-  pool_size: 15,
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
